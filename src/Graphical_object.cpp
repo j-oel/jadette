a5d3c0e07275b47240eddf29500bdb7ec7d5222b
Joel <69542025+j-oel@users.noreply.github.com>
2021-03-06 14:07:50 +0100

    Add 2021 as copyright year in source files.

--- src/Graphical_object.cpp
+++ src/Graphical_object.cpp
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-only
 // This file is part of Jadette.
-// Copyright (C) 2020 Joel Jansson
+// Copyright (C) 2020-2021 Joel Jansson
 // Distributed under GNU General Public License v3.0
 // See gpl-3.0.txt or <https://www.gnu.org/licenses/>
 
